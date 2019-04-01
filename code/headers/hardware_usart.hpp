934ae838d4f4b53215870197ad0d9c9a1d07b873
Olivier Verwoerd <olivier.verwoerd@student.hu.nl>
2019-04-01 20:20:57 +0200

    added .clang and fixed it in code
    now it should work. but it does make me wonder what clang was doing?

--- code/headers/hardware_usart.hpp
+++ code/headers/hardware_usart.hpp
@@ -6,102 +6,91 @@
 
 namespace r2d2 {
 
-class hardware_usart_c {
-private:
-  Usart *hardware_usart = nullptr;
-  unsigned int baudrate;
-  bool usart_initialized = true;
-  uart_ports_c &usart_port;
-  queue_c<uint8_t, 250> input_buffer;
+    class hardware_usart_c {
+    private:
+        Usart *hardware_usart = nullptr;
+        unsigned int baudrate;
+        bool usart_initialized = true;
+        uart_ports_c &usart_port;
+        queue_c<uint8_t, 250> input_buffer;
 
-  /// @brief check if the transmitter is ready to send
-  /// @return true if ready to send, false if not ready to send
-  bool transmit_ready();
+        /// @brief check if the transmitter is ready to send
+        /// @return true if ready to send, false if not ready to send
+        bool transmit_ready();
 
-  /// @brief send a byte via usart
-  /// @param b: byte to send
-  void send_byte(const uint8_t &b);
+        /// @brief send a byte via usart
+        /// @param b: byte to send
+        void send_byte(const uint8_t &b);
 
-  /// @brief receive a byte by reading the US_RHR register
-  /// @return byte received
-  uint8_t receive_byte();
+        /// @brief receive a byte by reading the US_RHR register
+        /// @return byte received
+        uint8_t receive_byte();
 
-public:
-  hardware_usart_c(unsigned int baudrate, uart_ports_c &usart_port);
+    public:
+        hardware_usart_c(unsigned int baudrate, uart_ports_c &usart_port);
 
-  /// console character output function
-  ///
-  /// This is the function used for console (ostream) output.
-  /// The embedded targets provide an implementation that writes
-  /// to the serial port.
-  ///
-  /// This definition is weak, which allows
-  /// an application to provide its own definition.
-  void uart_putc(char c);
+        /// console character output function
+        ///
+        /// This is the function used for console (ostream) output.
+        /// The embedded targets provide an implementation that writes
+        /// to the serial port.
+        ///
+        /// This definition is weak, which allows
+        /// an application to provide its own definition.
+        void uart_putc(char c);
 
-  /// @brief char output operator
-  ///
-  /// Although calling send_byte should do the exact same thing.
-  /// In practice useing this fuction is more stable
-  /// Especially when the values are repeated.
-  hardware_usart_c &operator<<(uint8_t byte) {
-    send_byte(byte);
-    return *this;
-  }
+        /// @brief char output operator
+        ///
+        /// Although calling send_byte should do the exact same thing.
+        /// In practice useing this fuction is more stable
+        /// Especially when the values are repeated.
+        hardware_usart_c &operator<<(uint8_t byte) {
+            send_byte(byte);
+            return *this;
+        }
 
-  /// @brief string output operator
-  ///
-  /// used for char arrays to quickly send more than one byte
-  /// In practice useing this fuction is more stable than repeated use of
-  /// send_byte() Especially when the values are repeated.
-  hardware_usart_c &operator<<(const char *c) {
-    hwlib::cout << c;
-    for (const char *p = c; *p != '\0'; p++) {
-      send_byte(*p);
-    }
-    return *this;
-  }
+        /// @brief string output operator
+        ///
+        /// used for char arrays to quickly send more than one byte
+        /// In practice useing this fuction is more stable than repeated use of
+        /// send_byte() Especially when the values are repeated.
+        hardware_usart_c &operator<<(const char *c) {
+            hwlib::cout << c;
+            for (const char *p = c; *p != '\0'; p++) {
+                send_byte(*p);
+            }
+            return *this;
+        }
 
-  // WIP? Getc works fine.
-  // char output operator
-  // template<>
-  //    basic_istream<char>&
-  //    operator>>(basic_istream<char>& __is, basic_string<char>& __str);
-  // hardware_usart_c &operator>>(std::string &output) {
-  //  char c = getc();
-  //  output += c;
-  //  return *this;
-  //}
+        /// @brief enables the internal USART controller
+        void enable();
 
-  /// @brief enables the internal USART controller
-  void enable();
+        /// @brief disables the internal USART controller
+        void disable();
 
-  /// @brief disables the internal USART controller
-  void disable();
+        /// @brief send single byte
+        /// @param c: byte to send
+        /// @return true if byte send, false if not send
+        bool send(const uint8_t c);
 
-  /// @brief send single byte
-  /// @param c: byte to send
-  /// @return true if byte send, false if not send
-  bool send(const uint8_t c);
+        /// @brief sends a char via usart
+        /// @param c: char to send
+        void putc(char c);
 
-  /// @brief sends a char via usart
-  /// @param c: char to send
-  void putc(char c);
+        /// @brief recieve byte bia usart
+        /// @return received byte
+        uint8_t receive();
 
-  /// @brief recieve byte bia usart
-  /// @return received byte
-  uint8_t receive();
+        /// @brief check if char is available
+        /// @return true if char is available false if not
+        bool char_available();
 
-  /// @brief check if char is available
-  /// @return true if char is available false if not
-  bool char_available();
+        /// @brief receive char via usart
+        /// @return char received
+        char getc();
 
-  /// @brief receive char via usart
-  /// @return char received
-  char getc();
-
-  /// @brief returns available data in buffer
-  /// @return amount of uint8_t's in buffer
-  unsigned int available();
-};
+        /// @brief returns available data in buffer
+        /// @return amount of uint8_t's in buffer
+        unsigned int available();
+    };
 }; // namespace r2d2
--- code/src/hardware_usart.cpp
+++ code/src/hardware_usart.cpp
@@ -3,115 +3,120 @@
 
 namespace r2d2 {
 
-bool hardware_usart_c::transmit_ready() { return (hardware_usart->US_CSR & 2); }
+    bool hardware_usart_c::transmit_ready() {
+        return (hardware_usart->US_CSR & 2);
+    }
 
-void hardware_usart_c::send_byte(const uint8_t &b) {
-  while (!transmit_ready()) {
-  }
-  hardware_usart->US_THR = b;
-  // hwlib::wait_ms(1);
-}
+    void hardware_usart_c::send_byte(const uint8_t &b) {
+        while (!transmit_ready()) {
+        }
+        hardware_usart->US_THR = b;
+        // hwlib::wait_ms(1);
+    }
 
-uint8_t hardware_usart_c::receive_byte() { return hardware_usart->US_RHR; }
+    uint8_t hardware_usart_c::receive_byte() {
+        return hardware_usart->US_RHR;
+    }
 
-hardware_usart_c::hardware_usart_c(unsigned int baudrate,
-                                   uart_ports_c &usart_port)
-    : baudrate(baudrate), usart_port(usart_port) {
-  if (usart_port == uart_ports_c::uart1) {
+    hardware_usart_c::hardware_usart_c(unsigned int baudrate,
+                                       uart_ports_c &usart_port)
+        : baudrate(baudrate), usart_port(usart_port) {
+        if (usart_port == uart_ports_c::uart1) {
 
-    hardware_usart = USART0;
+            hardware_usart = USART0;
 
-    PIOA->PIO_PDR = PIO_PA10;
-    PIOA->PIO_ABSR &= ~PIO_PA10;
-    PIOA->PIO_PDR = PIO_PA11;
-    PIOA->PIO_ABSR &= ~PIO_PA11;
+            PIOA->PIO_PDR = PIO_PA10;
+            PIOA->PIO_ABSR &= ~PIO_PA10;
+            PIOA->PIO_PDR = PIO_PA11;
+            PIOA->PIO_ABSR &= ~PIO_PA11;
 
-    PMC->PMC_PCER0 = (0x01 << ID_USART0);
-  } else if (usart_port == uart_ports_c::uart2) {
-    hardware_usart = USART1;
+            PMC->PMC_PCER0 = (0x01 << ID_USART0);
+        } else if (usart_port == uart_ports_c::uart2) {
+            hardware_usart = USART1;
 
-    PIOA->PIO_PDR = PIO_PA12;
-    PIOA->PIO_ABSR &= ~PIO_PA12;
-    PIOA->PIO_PDR = PIO_PA13;
-    PIOA->PIO_ABSR &= ~PIO_PA13;
+            PIOA->PIO_PDR = PIO_PA12;
+            PIOA->PIO_ABSR &= ~PIO_PA12;
+            PIOA->PIO_PDR = PIO_PA13;
+            PIOA->PIO_ABSR &= ~PIO_PA13;
 
-    PMC->PMC_PCER0 = (0x01 << ID_USART1);
-  } else if (usart_port == uart_ports_c::uart3) {
+            PMC->PMC_PCER0 = (0x01 << ID_USART1);
+        } else if (usart_port == uart_ports_c::uart3) {
 
-    hardware_usart = USART3;
+            hardware_usart = USART3;
 
-    PIOD->PIO_PDR = PIO_PD4;
-    PIOD->PIO_ABSR |= PIO_PD4;
-    PIOD->PIO_PDR = PIO_PD5;
-    PIOD->PIO_ABSR |= PIO_PD5;
+            PIOD->PIO_PDR = PIO_PD4;
+            PIOD->PIO_ABSR |= PIO_PD4;
+            PIOD->PIO_PDR = PIO_PD5;
+            PIOD->PIO_ABSR |= PIO_PD5;
 
-    PMC->PMC_PCER0 = (0x01 << ID_USART3);
-  } else if (usart_port == uart_ports_c::uart0) {
-    // NO! its probably not a good idea to use this, it is already in use by the
-    // hwlib::cout stuff
-    // hardware_usart = UART;
-    // PIOA->PIO_PDR   = PIO_PA8;
-    // PIOA->PIO_ABSR &= ~PIO_PA8;
-    // PIOA->PIO_PDR   = PIO_PA9;
-    // PIOA->PIO_ABSR &= ~PIO_PA9;
+            PMC->PMC_PCER0 = (0x01 << ID_USART3);
+        } else if (usart_port == uart_ports_c::uart0) {
+            // NO! its probably not a good idea to use this, it is already in
+            // use by the hwlib::cout stuff hardware_usart = UART; PIOA->PIO_PDR
+            // = PIO_PA8; PIOA->PIO_ABSR &= ~PIO_PA8; PIOA->PIO_PDR   = PIO_PA9;
+            // PIOA->PIO_ABSR &= ~PIO_PA9;
 
-    // PMC->PMC_PCER0 = ( 0x01 << ID_UART );*/
-    usart_initialized = false;
-  }
+            // PMC->PMC_PCER0 = ( 0x01 << ID_UART );*/
+            usart_initialized = false;
+        }
 
-  disable();
+        disable();
 
-  hardware_usart->US_BRGR = (5241600u / baudrate);
+        hardware_usart->US_BRGR = (5241600u / baudrate);
 
-  hardware_usart->US_MR =
-      UART_MR_PAR_NO | UART_MR_CHMODE_NORMAL | US_MR_CHRL_8_BIT;
-  hardware_usart->US_IDR = 0xFFFFFFFF;
+        hardware_usart->US_MR =
+            UART_MR_PAR_NO | UART_MR_CHMODE_NORMAL | US_MR_CHRL_8_BIT;
+        hardware_usart->US_IDR = 0xFFFFFFFF;
 
-  enable();
-}
+        enable();
+    }
 
-void hardware_usart_c::enable() {
-  if (hardware_usart) {
-    hardware_usart->US_CR = UART_CR_RXEN | UART_CR_TXEN;
-  }
-}
+    void hardware_usart_c::enable() {
+        if (hardware_usart) {
+            hardware_usart->US_CR = UART_CR_RXEN | UART_CR_TXEN;
+        }
+    }
 
-void hardware_usart_c::disable() {
-  if (hardware_usart) {
-    hardware_usart->US_CR =
-        UART_CR_RSTRX | UART_CR_RSTTX | UART_CR_RXDIS | UART_CR_TXDIS;
-  }
-}
+    void hardware_usart_c::disable() {
+        if (hardware_usart) {
+            hardware_usart->US_CR =
+                UART_CR_RSTRX | UART_CR_RSTTX | UART_CR_RXDIS | UART_CR_TXDIS;
+        }
+    }
 
-bool hardware_usart_c::send(const uint8_t c) {
-  send_byte(c);
-  return true;
-}
+    bool hardware_usart_c::send(const uint8_t c) {
+        send_byte(c);
+        return true;
+    }
 
-void hardware_usart_c::putc(char c) { send_byte(c); }
+    void hardware_usart_c::putc(char c) {
+        send_byte(c);
+    }
 
-uint8_t hardware_usart_c::receive() {
-  if (!input_buffer.size()) {
-    return 0;
-  }
+    uint8_t hardware_usart_c::receive() {
+        if (!input_buffer.size()) {
+            return 0;
+        }
 
-  return input_buffer.copy_and_pop();
-}
+        return input_buffer.copy_and_pop();
+    }
 
-bool hardware_usart_c::char_available() { return (available() > 0); }
+    bool hardware_usart_c::char_available() {
+        return (available() > 0);
+    }
 
-char hardware_usart_c::getc() {
-  if (available() > 0) {
-    return receive();
-  }
-  return 0;
-}
+    char hardware_usart_c::getc() {
+        if (available() > 0) {
+            return receive();
+        }
+        return 0;
+    }
 
-unsigned int hardware_usart_c::available() {
-  if ((hardware_usart->US_CSR & 1) != 0) {
-    input_buffer.push(receive_byte());
-  }
+    unsigned int hardware_usart_c::available() {
+        if ((hardware_usart->US_CSR & 1) != 0) {
+            input_buffer.push(receive_byte());
+        }
 
-  return input_buffer.size();
-}
+        return input_buffer.size();
+    }
 }; // namespace r2d2
