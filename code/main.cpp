921ba4f692b5ad6a6a3d85d2852fac90c0332834
itzandroidtab <koendegraaff@itzandroidtab.nl>
2019-05-02 21:53:18 +0200

    added a example on usart1

--- code/main.cpp
+++ code/main.cpp
@@ -1,7 +1,6 @@
 #include <hwlib.hpp>
 
 #include <hardware_usart.hpp>
-#include <test_usart.hpp>
 
 int main(void) {
   // kill the watchdog
@@ -9,4 +8,13 @@ int main(void) {
     hwlib::wait_ms(1000);
     hwlib::cout << "this works on arduino";
     // nothing of use here
+
+    r2d2::hardware_usart_c usart(115200, r2d2::uart_ports_c::uart1);
+
+    uint16_t count = 0;
+
+    for(;;){
+        usart << "Test: " << count++ << '\n';
+        hwlib::wait_ms(100);
+    }
 }
