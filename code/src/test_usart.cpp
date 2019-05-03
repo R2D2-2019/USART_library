a9a651b1c2de161e7b3f84c4b4cba256ca6dd628
itzandroidtab <koendegraaff@itzandroidtab.nl>
2019-05-03 15:13:37 +0200

    made the test run again

--- code/src/test_usart.cpp
+++ code/src/test_usart.cpp
@@ -1,20 +1,20 @@
 #include <test_usart.hpp>
 
 namespace r2d2::usart {
-    test_usart_c::test_usart_c(){
+
+    test_usart_c::test_usart_c(){}
+
+    void test_usart_c::enable(){
+        //Not needed in test implementation
     }
 
-    void test_usart_c::enable() {
-        // Not needed in test implementation
-    }
-
-    void test_usart_c::disable() {
-        // Not needed in test implementation
+    void test_usart_c::disable(){
+        //Not needed in test implementation
     }
 
     bool test_usart_c::send(const uint8_t c) {
-        // Not needed in test implementation
-        return false;
+        return true;
+        //Not needed in test implementation
     }
 
     void test_usart_c::putc(char c) {
@@ -22,11 +22,11 @@ namespace r2d2::usart {
     }
 
     uint8_t test_usart_c::receive() {
-        return 0xAA;
+        return receive_buffer.copy_and_pop();
     }
 
     bool test_usart_c::char_available() {
-        return true;
+        return !receive_buffer.empty();
     }
 
     char test_usart_c::getc() {
@@ -34,6 +34,24 @@ namespace r2d2::usart {
     }
 
     unsigned int test_usart_c::available() {
-        return 1;
+        return receive_buffer.size();
     }
+
+    void test_usart_c::set_receive_string(const std::string &str) {
+        receive_buffer.clear();
+        for(char c : str){
+            add_receive_byte(c);
+        }
+    }
+
+    void test_usart_c::set_receive_bytes(const std::vector<uint8_t> &bytes){
+        receive_buffer.clear();
+        for(auto &byte : bytes){
+            add_receive_byte(byte);
+        }
+    }
+
+    void test_usart_c::add_receive_byte(const uint8_t byte){
+        receive_buffer.push(byte);
+    }    
 }; // namespace r2d2::usart
--- test/main.cpp
+++ test/main.cpp
@@ -5,16 +5,15 @@
 #include <catch.hpp>
 
 #include <test_usart.hpp>
-#include <uart_ports.hpp>
 
 TEST_CASE("test_usart_c sends","[test_usart_c]"){
-    auto test_usart = r2d2::test_usart_c();
+    auto test_usart = r2d2::usart::test_usart_c();
 
     REQUIRE(test_usart.send(0xAA));    
 }
 
 TEST_CASE("test_usart_c chars availiable ", "[test_usart_c]"){
-    auto test_usart = r2d2::test_usart_c();
+    auto test_usart = r2d2::usart::test_usart_c();
    
     REQUIRE(!test_usart.char_available());
 
@@ -24,7 +23,7 @@ TEST_CASE("test_usart_c chars availiable ", "[test_usart_c]"){
 }
 
 TEST_CASE("test_usart_c receives", "[test_usart_c]"){
-    auto test_usart = r2d2::test_usart_c();
+    auto test_usart = r2d2::usart::test_usart_c();
     test_usart.add_receive_byte(0x25);
 
     REQUIRE(test_usart.receive() == 0x25);
@@ -32,14 +31,14 @@ TEST_CASE("test_usart_c receives", "[test_usart_c]"){
 }
 
 TEST_CASE("test_usart_c is empty after receiving", "[test_usart_c]"){
-    auto test_usart = r2d2::test_usart_c();
+    auto test_usart = r2d2::usart::test_usart_c();
     test_usart.add_receive_byte(0x25);
     test_usart.receive();
     REQUIRE(!test_usart.available());
 }
 
 TEST_CASE("set_receive_string()","[test_usart_c]"){
-    auto test_usart = r2d2::test_usart_c();
+    auto test_usart = r2d2::usart::test_usart_c();
     std::string str1 = "this is a test string";
     std::string str2;
     test_usart.set_receive_string(str1);
@@ -50,7 +49,7 @@ TEST_CASE("set_receive_string()","[test_usart_c]"){
 }
 
 TEST_CASE("set_receive_bytes()","[test_usart_c]"){
-    auto test_usart = r2d2::test_usart_c();
+    auto test_usart = r2d2::usart::test_usart_c();
     std::vector<uint8_t> vec1 = {0xAA, 0x25, 0x01, 0x56};
     std::vector<uint8_t> vec2;
     test_usart.set_receive_bytes(vec1);
