7c17c619d668e9824a3b123ac1451568c6249c4e
Cris <crisvdnolle@hotmail.com>
2019-06-26 10:48:14 +0200

    Edited function, Removed function after feedback
    Removed a function to set the send buffer with a string. This function was removed after feedback due to the fact that this function might not be used at all. Edited a function so it uses the send function instead of the add_send_byte. Added get_send_bytes function which returns the send buffer into a vector.

--- code/headers/test_usart.hpp
+++ code/headers/test_usart.hpp
@@ -28,9 +28,9 @@ namespace r2d2::usart {
     public:
         /**
          * @brief Write a uint8_t with the usart
-         * 
+         *
          * @details will add the byte to the send buffer.
-         * 
+         *
          * @param byte
          */
         void send(const uint8_t byte) override {
@@ -72,20 +72,8 @@ namespace r2d2::usart {
         }
 
         /**
-         * @brief Set a string the test usart will return
-         *
-         * @param str
-         */
-        void set_send_string(const std::string &str) {
-            send_buffer.clear();
-
-            for (char c : str) {
-                send(c);
-            }
-        }
-
-        /**
-         * @brief Set the bytes to the receive buffer that the test usart will return
+         * @brief Set the bytes to the receive buffer that the test usart will
+         * return
          *
          * @param bytes
          */
@@ -98,7 +86,8 @@ namespace r2d2::usart {
         }
 
         /**
-         * @brief Add a byte to the receive buffer data that the test usart will return
+         * @brief Add a byte to the receive buffer data that the test usart will
+         * return
          *
          * @param byte
          */
@@ -113,23 +102,15 @@ namespace r2d2::usart {
          */
         void set_send_bytes(const std::vector<uint8_t> &bytes) {
             send_buffer.clear();
-            
-            for(auto &byte : bytes){
-                add_send_byte(byte);
+
+            for (auto &byte : bytes) {
+                send(byte);
             }
         }
 
         /**
-         * @brief Add a byte to the send buffer that the test usart will return
-         *
-         * @param byte
-         */
-        void add_send_byte(const uint8_t byte){
-            send_buffer.push_back(byte);
-        }
-
-        /**
-         * @brief returns and removes the first byte that was added into the send buffer.
+         * @brief returns and removes the first byte that was added into the
+         * send buffer.
          *
          * @return uint8_t
          */
@@ -138,6 +119,15 @@ namespace r2d2::usart {
             send_buffer.erase(send_buffer.begin());
 
             return byte;
+        }
+
+        /**
+         * @brief returns the send buffer into a vector.
+         *
+         * return std::vector<uint8_t>
+         */
+        std::vector<uint8_t> get_send_bytes() const {
+            return send_buffer;
         }
     };
 }; // namespace r2d2::usart
